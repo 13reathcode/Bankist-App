f298ed2c90ac2c2dfcdf16565928179cc07e6605
13reath <new13reath@gmail.com>
2023-02-12 16:39:22 +0200

    Updated movements dates

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ const account1 = {
     '2022-04-01T10:17:24.185Z',
     '2022-05-08T14:11:59.604Z',
     '2022-05-27T17:01:17.194Z',
-    '2022-07-11T23:36:17.929Z',
-    '2022-07-12T10:51:36.790Z',
+    '2023-02-11T23:36:17.929Z',
+    '2023-02-12T10:51:36.790Z',
   ],
   currency: 'UAH',
   locale: 'uk_UA',
@@ -93,6 +93,26 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// FUNCTIONS
+
+const formatMovementDate = date => {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0),
+      month = `${date.getMonth() + 1}`.padStart(2, 0),
+      year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+};
+
 const displayMovements = function (account, sort = false) {
   // Clearing movements div
   containerMovements.innerHTML = '';
@@ -105,12 +125,8 @@ const displayMovements = function (account, sort = false) {
   sortedMovs.forEach((movement, index) => {
     const type = movement > 0 ? 'deposit' : 'withdrawal';
 
-    const date = new Date(account.movementsDates[index]),
-      day = `${date.getDate()}`.padStart(2, 0),
-      month = `${date.getMonth() + 1}`.padStart(2, 0),
-      year = date.getFullYear();
-
-    const displayDate = `${day}/${month}/${year}`;
+    const date = new Date(account.movementsDates[index]);
+    const displayDate = formatMovementDate(date);
 
     // prettier-ignore
     const html = `
